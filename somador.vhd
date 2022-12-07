dbf47158f50f0b729e9c2bbb566cf89021b5c122
NatalyAlmeida <116127521+NatalyAlmeida@users.noreply.github.com>
2022-12-06 20:35:23 -0400

    Create somador.vhd

---
+++ somador.vhd
@@ -0,0 +1,23 @@
+library ieee;
+use ieee.std_logic_1164.all;
+USE ieee.std_logic_unsigned.all;
+
+entity somador8 is
+    port(
+        saida1    : in std_logic_vector(7 downto 0);
+
+        saidasoma  : out std_logic_vector(7 downto 0)
+    );
+end somador8;
+
+architecture soma of somador8 is
+
+	 signal pertode: std_logic_vector(7 downto 0);
+	 
+    begin 
+        process (saida1)
+        begin
+            pertode <= "00000001";
+            saidasoma <= saida1 + pertode;
+        end process;
+end soma;
